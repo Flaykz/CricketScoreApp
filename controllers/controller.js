8f5e649ad3060348ca04c5baa9db69e657757562
root <root@vps203083.ovh.net>
2017-11-03 10:19:24 +1100

    PageSpeed OK

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,14 +1,26 @@
+var mcache = require("memory-cache");
+
+var cache = (duration) => {
+  return (req, res, next) => {
+    let key = '__express__' + req.originalUrl || req.url
+    let cachedBody = mcache.get(key)
+    if (cachedBody) {
+      res.send(cachedBody)
+      return
+    } else {
+      res.sendResponse = res.send
+      res.send = (body) => {
+        mcache.put(key, body, duration * 1000);
+        res.sendResponse(body)
+      }
+      next()
+    }
+  }
+}
+
 module.exports.controller = function(app) {
-    // add other routes below
-    app.get('/about', function (req, res) {
-        //res.sendFile(path.join(__dirname + 'views/about.html'));
-        res.render('about', {
-            title: 'About CricketScoreApp'
-        });
-    });
-    
     // viewed at based directory http://localhost:8080/
-    app.get('/', function (req, res) {
+    app.get('/', cache(10), function (req, res) {
         res.render('home', {
             title: 'CricketScoreApp',
             pageData: {item: ['', '=', '20', '19', '18', '17', '16', '15', 'Bull'], name: ['', 'Joueur_1']}
--- server.js
+++ server.js
@@ -1,14 +1,22 @@
+#!/usr/bin/env nodejs
 var express = require('express');
 var fs = require('fs');
 var app = express();
+var morgan = require("morgan");
 var path = require('path');
+var compression = require("compression");
+//var zlib = require("zlib");
 
 app.set('views', './views');
 app.set('view engine', 'jade');
 
+var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), {flags: 'a'});
+
+app.use(morgan(':date[clf] :remote-addr ":method :url HTTP/:http-version" :status :req[header] :response-time ms ":referrer" - ":user-agent"', {stream: accessLogStream}));
+app.use(compression());
 app.use(express.static(path.join(__dirname)));
 app.use("/styles", express.static(__dirname + '/styles'));
-app.use("/images", express.static(__dirname + '/images'));
+app.use("/images", express.static(__dirname + '/images', {maxAge: 86400000}));
 //app.use("/controllers", express.static(__dirname + '/controllers'));
 //app.use("/models", express.static(__dirname + '/models'));
 
@@ -19,4 +27,4 @@ fs.readdirSync('./controllers').forEach(function (file) {
   }
 });
 
-app.listen(8080);
+app.listen(8080,"localhost");
