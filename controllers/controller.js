d7ab0fe9ee632cfb39cbf8bdf26a7da6b8f69398
Flaykz <david.brouste.perso@gmail.com>
2018-02-16 11:13:52 +1100

    suppression de mcache

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,27 +1,8 @@
-var mcache = require("memory-cache");
 var config = require('../config.js').get(process.env.NODE_ENV);
-
-var cache = (duration) => {
-  return (req, res, next) => {
-    let key = '__express__' + req.originalUrl || req.url
-    let cachedBody = mcache.get(key)
-    if (cachedBody) {
-      res.send(cachedBody)
-      return
-    } else {
-      res.sendResponse = res.send
-      res.send = (body) => {
-        mcache.put(key, body, duration * 1000);
-        res.sendResponse(body)
-      }
-      next()
-    }
-  }
-}
 
 module.exports.controller = function(app) {
   // viewed at based directory http://localhost:8080/
-  app.get('/', cache(10), function (req, res) {
+  app.get('/', function (req, res) {
     var len = Object.keys(req.query).length;
     if (len > 0) {
       var params = [];
