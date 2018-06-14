762d7f99d88e9248219e6df11de53c545f6acea4
Flaykz <david.brouste.perso@gmail.com>
2018-06-15 10:57:45 +1100

    add cricket v2 config

--- config.js
+++ config.js
@@ -6,6 +6,13 @@ var config = {
         "item": ['', '=', '20', '19', '18', '17', '16', '15', 'Bull'],
         "name": ['', 'Joueur_1', 'Joueur_2', 'Joueur_3', 'Joueur_4']
     },
+    productionv2: {
+        "PORT": 8088,
+        "title": "CricketV2",
+        "colour": ['#ffff00', '#00ff00', '#00ffff', '#0000ff'],
+        "item": ['', '=', '20', '19', '18', '17', '16', '15', 'Bull'],
+        "name": ['', 'Joueur_1', 'Joueur_2', 'Joueur_3', 'Joueur_4']
+    },
     default: {
         "PORT": 8080,
         "title": "dev",
