f124204d01b26941a7b3917510f82854e83a8e59
Flaykz <david.brouste.perso@gmail.com>
2019-05-05 23:54:00 +1100

    fix port

--- config.js
+++ config.js
@@ -1,27 +1,27 @@
 var config = {
-    production: {
+    cricket: {
         "PORT": 8087,
         "title": "CricketScore",
         "colour": ['#ffff00', '#00ff00', '#00ffff', '#0000ff'],
         "item": ['', '=', '20', '19', '18', '17', '16', '15', 'Bull'],
         "name": ['', 'Joueur_1', 'Joueur_2', 'Joueur_3', 'Joueur_4']
     },
-    productionv2: {
-        "PORT": 8087,
-        "title": "CricketScore",
-        "colour": ['#ffff00', '#00ff00', '#00ffff', '#0000ff'],
-        "item": ['', '=', '20', '19', '18', '17', '16', '15', 'Bull'],
-        "name": ['', 'Joueur_1', 'Joueur_2', 'Joueur_3', 'Joueur_4']
-    },
-    productionv2: {
+    cricketv2: {
         "PORT": 8088,
         "title": "CricketV2",
         "colour": ['#ffff00', '#00ff00', '#00ffff', '#0000ff'],
         "item": ['', '=', '20', '19', '18', '17', '16', '15', 'Bull'],
         "name": ['', 'Joueur_1', 'Joueur_2', 'Joueur_3', 'Joueur_4']
     },
+    dev: {
+        "PORT": 8080,
+        "title": "CricketV2",
+        "colour": ['#ffff00', '#00ff00', '#00ffff', '#0000ff'],
+        "item": ['', '=', '20', '19', '18', '17', '16', '15', 'Bull'],
+        "name": ['', 'Joueur_1', 'Joueur_2', 'Joueur_3', 'Joueur_4']
+    },
     default: {
-        "PORT": 8087,
+        "PORT": 8080,
         "title": "CricketScore",
         "colour": ['#ffff00', '#00ff00', '#00ffff', '#0000ff'],
         "item": ['', '=', '20', '19', '18', '17', '16', '15', 'Bull'],
